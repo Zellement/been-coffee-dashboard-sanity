73429f5d15e17783a697642c95ffd84cb08a46a6
Dan Farrow <dan@zellement.com>
2024-03-17 15:53:49 +0000

    store og

--- schemaTypes/article.ts
+++ schemaTypes/article.ts
@@ -37,7 +37,7 @@ export default defineType({
           type: 'file',
           name: 'file',
           options: {
-            originalFilename: true
+            storeOriginalFilename: true
           }
           // fields: [
           //   {
