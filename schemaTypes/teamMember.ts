94cfd323b8870a3ffe87277520f49254f4a17dfe
Dan Farrow <dan@zellement.com>
2024-06-15 10:56:25 +0100

    remove required dob

--- schemaTypes/teamMember.ts
+++ schemaTypes/teamMember.ts
@@ -42,7 +42,6 @@ export default defineType({
       options: {
         dateFormat: 'Do MMMM'
       },
-      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'slug',
