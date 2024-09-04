9c50a407b1978524b76a31502367a2f4861eed5c
Dan Farrow <dan@zellement.com>
2024-09-04 08:09:33 +0100

    add studioHost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  studioHost: 'been-coffee-dashboard',
   api: {
     projectId: 'mxklvbih',
     dataset: 'production'
